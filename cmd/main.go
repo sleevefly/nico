2733ad8763728d7740289c6791dcd4c37109a16d
d9887987 <599907482@qq.com>
2023-12-01 18:47:25 +0800

    添加日志信息

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"github.com/gin-gonic/gin"
-	"log"
+	"github.com/sirupsen/logrus"
 	"net/http"
+	"nico/conf"
 	"os"
 	"os/signal"
 	"syscall"
@@ -14,6 +15,7 @@ import (
 func main() {
 	r := gin.Default()
 	gin.SetMode("release")
+	conf.InitLog()
 
 	r.GET("/ping", func(c *gin.Context) {
 		time.Sleep(5 * time.Second)
@@ -28,7 +30,7 @@ func main() {
 	// Initializing the server in a goroutine so that it won't block the graceful shutdown handling below
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
+			logrus.Fatalf("listen: %s\n", err)
 		}
 	}()
 
@@ -39,14 +41,14 @@ func main() {
 	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	log.Println("Shutting down server...")
+	logrus.Println("Shutting down server...")
 
 	// The context is used to inform the server it has 5 seconds to finish the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown: ", err)
+		logrus.Fatal("Server forced to shutdown: ", err)
 	}
 
-	log.Println("Server exiting")
+	logrus.Println("Server exiting")
 }
