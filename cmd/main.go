2265095b5ac7922621c6b3a6998c5e71e4cb1089
d9887987 <599907482@qq.com>
2023-12-06 17:52:03 +0800

    删除无用日志

--- cmd/main.go
+++ cmd/main.go
@@ -50,11 +50,6 @@ func main() {
 	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
-	logrus.Debug("Shutting down server...")
-	logrus.Warn("Shutting down server..")
-	logrus.Info("Shutting down server..")
-	logrus.Error("Shutting down server..")
-
 	// The context is used to inform the server it has 5 seconds to finish the request it is currently handling
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
